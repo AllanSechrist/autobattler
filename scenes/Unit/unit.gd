35506795cfcf71deaadb132ec3f5f613b19a45bb
AllanSechrist <sechristallan@gmail.com>
2025-04-10 07:52:59 +0900

    Start creation of sell portal for units.

--- scenes/Unit/unit.gd
+++ scenes/Unit/unit.gd
@@ -2,6 +2,8 @@
 class_name Unit
 extends Area2D
 
+signal quick_sell_pressed
+
 @export var stats: UnitStats : set = set_stats
 
 @onready var skin: Sprite2D = $Visuals/Skin
@@ -11,10 +13,19 @@ extends Area2D
 @onready var velocity_based_rotation: VelocityBasedRotation = $VelocityBasedRotation
 @onready var outline_highlighter: OutlineHighlighter = $OutlineHighlighter
 
+var is_hovered := false
+
 func _ready() -> void:
 	if not Engine.is_editor_hint():
 		drag_and_drop.drag_started.connect(_on_drag_started)
 		drag_and_drop.drag_canceled.connect(_on_drag_canceled)
+		
+func _input(event: InputEvent) -> void:
+	if not is_hovered:
+		return
+	
+	if event.is_action_pressed("quick_sell"):
+		quick_sell_pressed.emit()
 
 
 func set_stats(value: UnitStats) -> void:
@@ -43,7 +54,8 @@ func _on_mouse_entered() -> void:
 	# exit function if a player is dragging a unit.
 	if drag_and_drop.dragging:
 		return
-		
+	
+	is_hovered = true
 	outline_highlighter.highlight()
 	z_index = 1
 
@@ -52,5 +64,6 @@ func _on_mouse_exited() -> void:
 	if drag_and_drop.dragging:
 		return
 		
+	is_hovered = false
 	outline_highlighter.clear_highlight()
 	z_index = 0
